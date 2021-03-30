e8728d878f71fc1af92dc690de2eb3c410816044
beradi06 <gokgoz.berat123@gmail.com>
2021-03-30 16:33:56 +0300

    Github username change

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="tdk-sozluk",
-    version="1.0.3",
+    version="1.0.4",
     author="Berat Gökgöz",
     author_email="gokgoz.berat123@gmail.com",
     description="En iyi TDK sözlük arama kütüphanesi. Best TDK dict. search library.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/beradi06/tdk-sozluk",
+    url="https://github.com/electromeow/tdk-sozluk",
     project_urls={
-        "Bug Tracker": "https://github.com/beradi06/tdk-sozluk/issues",
+        "Bug Tracker": "https://github.com/electromeow/tdk-sozluk/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
